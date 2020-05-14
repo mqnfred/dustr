57ddfb22d1281085c6a18b9a2e5a7184c1499fd2
Louis Feuvrier <mqnfred@gmail.com>
2020-05-14 11:43:08 -0700

    Add strings test case

--- tests/strings/bin/main.dart
+++ tests/strings/bin/main.dart
@@ -3,5 +3,7 @@ import 'package:strings/strings.dart';
 void main() {
 	var addr = Address.build("hello", "world", "!");
 	print("${addr.line} ${addr.city} ${addr.country}");
+	addr = setAddressLine(addr, "coucou");
+	print("${addr.line} ${addr.city} ${addr.country}");
 	addr.free();
 }
