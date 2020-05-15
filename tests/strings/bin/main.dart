ca0a3ec7bb48ff9e991bd92bd7026d9b7c2b0319
Louis Feuvrier <mqnfred@gmail.com>
2020-05-15 15:30:57 -0700

    Expose bug in new strings test

--- tests/strings/bin/main.dart
+++ tests/strings/bin/main.dart
@@ -3,7 +3,13 @@ import 'package:strings/strings.dart';
 void main() {
 	var addr = Address.build("hello", "world", "!");
 	print("${addr.line} ${addr.city} ${addr.country}");
+
 	addr = setAddressLine(addr, "coucou");
 	print("${addr.line} ${addr.city} ${addr.country}");
-	addr.free();
+
+	// TODO: the dart side leaks this string in the wrapper
+	// function. this is a real bug that needs fixing. would
+	// likely imply wrapping the string on the dart side
+	var line = getAddressLine(addr);
+	print("${line}");
 }
--- tests/strings/src/lib.rs
+++ tests/strings/src/lib.rs
@@ -1,7 +1,6 @@
 #[macro_use]
 extern crate ffishim_derive;
 #[ffishim_library]
-
 #[derive(FFIShim)]
 pub struct Address {
     line: String,
@@ -13,4 +12,12 @@ pub struct Address {
 fn set_address_line(mut addr: Address, line: String) -> Address {
     addr.line = line;
     addr
+}
+
+// TODO: the dart side leaks this string in the wrapper
+// function. this is a real bug that needs fixing. would
+// likely imply wrapping the string on the dart side
+#[ffishim_use_case]
+fn get_address_line(addr: Address) -> String {
+    addr.line
 }
