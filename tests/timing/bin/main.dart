22d37f34ec69b5f4fc95cd4675dc70da2c891668
Louis Feuvrier <mqnfred@gmail.com>
2020-05-23 16:48:57 -0700

    Buff duration tests

--- tests/timing/bin/main.dart
+++ tests/timing/bin/main.dart
@@ -3,7 +3,12 @@ import 'package:timing/timing.dart';
 void main() {
 	var dur = getDaysDurationInMs(5);
 	print("${dur}");
-	var wr = WorldRecord.build(dur);
-	print("${wr.time}");
-	wr.free();
+
+	var wr1 = WorldRecord.build(dur);
+	print("${wr1.time}");
+	wr1.free();
+
+	var wr2 = WorldRecord.build(Duration(milliseconds: 5));
+	print("${wr2.time}");
+	wr2.free();
 }
